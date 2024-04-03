ae429b8f9823ea1fae7817aa010170e1eea7e0ef
Roman Bondarenko <theuargb@gmail.com>
2024-04-03 14:57:59 +0300

    feat(poc): [wip] poc

--- src/JustCheckMeOut/ViewModel/HeadlessComponentRenderer.php
+++ src/JustCheckMeOut/ViewModel/HeadlessComponentRenderer.php
@@ -9,20 +9,25 @@ class HeadlessComponentRenderer implements ArgumentInterface
 {
 
     public function __construct(
-        private readonly TemplateFactory $blockFactory
+        private readonly TemplateFactory $blockFactory,
     )
     {
     }
 
-    public function render(string $template, array $data = []): string
+    public function render(string $template, array $data = [], ?string $slug = null): string
     {
         $template = !str_contains($template, '::')
             ? "PerspectiveTeam_JustCheckMeOut::component/headless/$template.phtml"
             : $template;
 
+        if (!$slug) {
+            $slug = uniqid($template);
+        }
+
         return $this->blockFactory
             ->create()
             ->setTemplate($template)
+            ->setNameInLayout("psteam_justcheckmeout.headless.$slug")
             ->setData(array_merge([
                 'headlessComponentRenderer' => $this
             ], $data))

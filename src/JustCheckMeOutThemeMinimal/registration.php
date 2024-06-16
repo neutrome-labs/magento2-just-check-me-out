44f4fdd2cb1efb1219f86a3e9d2a40404c77b4e8
Roman Bondarenko <theuargb@gmail.com>
2024-06-16 23:20:31 +0300

    feat(checkout): minimal theme removed

--- src/JustCheckMeOutThemeMinimal/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'PerspectiveTeam_JustCheckMeOutThemeMinimal',
-    __DIR__
-);
